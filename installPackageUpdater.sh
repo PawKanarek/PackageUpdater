ac90a2feebf5c61c9e22c6086d40c04ea1b9f8d9
pkanarek <p.kanarek@insys.pl>
2020-05-04 23:07:18 +0200

    Added installation script on mac

---
+++ installPackageUpdater.sh
@@ -0,0 +1,13 @@
+#!/bin/zsh
+
+curl -o PackageUpdater -L https://github.com/PawKanarek/PackageUpdater/releases/download/1.2-mac/PackageUpdater
+
+cp PackageUpdater /usr/local/bin/PackageUpdater
+
+rm PackageUpdater
+
+chmod +x /usr/local/bin/PackageUpdater
+
+PackageUpdater -h
+
+rm installPackageUpdater.sh
